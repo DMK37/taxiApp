368fcb331c8b6e01fbd306f86868eb58256b33e0
dima <dimadmk9991@gmail.com>
2024-11-03 17:33:00 +0100

    init ios metamask integration

--- user_taxi_app/lib/auth/repository/metamask_repository.dart
+++ user_taxi_app/lib/auth/repository/metamask_repository.dart
@@ -1,12 +1,16 @@
+import 'package:flutter/services.dart';
 import 'package:taxiapp/auth/repository/auth_repository.dart';
 import 'package:taxiapp/models/user_model.dart';
 
 class MetamaskRepository implements AuthRepository {
+  static const platform = MethodChannel('metamask');
+
   @override
   Future<void> logout() async {}
 
   @override
   Future<UserModel> signIn() async {
+    await platform.invokeMethod('connectToMetaMask');
     return UserModel(
       walletId: "0x1234567890",
     );
